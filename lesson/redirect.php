de7f323ba8568a7fe55ad14df4bd8afc6a5a7d31
Ayame <166793391+ohiayame@users.noreply.github.com>
2025-01-15 18:40:24 +0900

    버튼과 문자 링크의 차이

--- lesson/redirect.php
+++ lesson/redirect.php
@@ -1,12 +1,17 @@
 <?php
-// POST 요청인지 확인
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-// 입력 데이터를 가져오고, HTML 특수 문자를 이스케이프
-$target = htmlspecialchars($_POST['target'], ENT_QUOTES, 'UTF-8');
-
-// 리다이렉트 대상 URL을 배열로 정의
+// 요청 확인
+// 버튼 : POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // 입력 데이터를 가져오고, HTML 특수 문자를 이스케이프
+    $target = htmlspecialchars($_POST['target'], ENT_QUOTES, 'UTF-8');
+// 문자 : GET
+} else {
+    $target = htmlspecialchars($_GET['target'] ?? '', ENT_QUOTES, 'UTF-8');
+}
+// target의 값 (target의 value)
 $redirect_urls = [
     'yju' => 'https://gsc.yju.ac.kr',
+    'yjp' => 'https://www.yjp.ac.kr/portal/main/index.jsp',
 ];
 
 // 대상 값이 배열에 있는지 확인
@@ -18,8 +23,5 @@ if (array_key_exists($target, $redirect_urls)) {
     // 유효하지 않은 요청에 대한 응답
     echo "無効なリクエストです。";
 }
-// } else {
-//     // POST 요청이 아닌 경우
-//     echo "このページには直接アクセスできません。";
-// }
+
 ?>
